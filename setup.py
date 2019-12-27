b1ea0da652df9cad24226d49d0987c3b6519c5fb
benman1 <auffarth@gmail.com>
2019-12-27 14:47:16 +0000

    pep8 compliance

--- setup.py
+++ setup.py
@@ -10,11 +10,13 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['numpy', 'pandas', 'scikit-learn', 'category_encoders>=2.0.0']
+requirements = [
+    'numpy', 'pandas', 'scikit-learn', 'category_encoders>=2.0.0', 'numba'
+]
 
-setup_requirements = [ ]
+setup_requirements = []
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
     author="Ben Auffarth",
@@ -31,14 +33,21 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
-    description="This contains all the steps needed to reproduce the OpenML pipeline steps",
+    description="""This contains all the steps needed to
+                    reproduce the OpenML pipeline steps
+                """,
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='openml_speed_dating_pipeline_steps',
     name='openml_speed_dating_pipeline_steps',
-    packages=find_packages(include=['openml_speed_dating_pipeline_steps', 'openml_speed_dating_pipeline_steps.*']),
+    packages=find_packages(
+        include=[
+            'openml_speed_dating_pipeline_steps',
+            'openml_speed_dating_pipeline_steps.*'
+        ]
+    ),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
