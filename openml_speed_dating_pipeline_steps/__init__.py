d835f35cdcfcd4daba5a51730a2bdeea2ecad9c8
benman1 <auffarth@gmail.com>
2019-12-27 14:47:43 +0000

    Bump version: 0.5.1 → 0.5.2

--- openml_speed_dating_pipeline_steps/__init__.py
+++ openml_speed_dating_pipeline_steps/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Ben Auffarth"""
 __email__ = 'auffarth@gmail.com'
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 
 from .openml_speed_dating_pipeline_steps import (
     RangeTransformer, FloatTransformer,
