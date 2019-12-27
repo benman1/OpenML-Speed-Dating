b3956c6f291b29faa0e446c5cc2e791ff1a1f716
benman1 <auffarth@gmail.com>
2019-12-27 15:46:23 +0000

    Bump version: 0.5.2 → 0.5.3

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/benman1/openml_speed_dating_pipeline_steps',
-    version='0.5.2',
+    version='0.5.3',
     zip_safe=False,
 )
